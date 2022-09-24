2298ea37f32c9b37b65f307a2c4e3e7884182892
serhiiarkhypiuk <airborn.gnus@gmail.com>
2022-09-24 11:36:48 +0300

    Initial commit

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,21 @@
-export const App = () => {
+import React from 'react';
+import ContactForm from './organisms/Form/Form';
+import Title from './atoms/Title/Title';
+import Filter from './atoms/Filter/Filter';
+import { StyledWrapper } from './organisms/Form/Form.styled';
+import ContactsList from "./atoms/ContactsList/ContactsList";
+
+const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <StyledWrapper>
+      <Title headerTitle="Phonebook" />
+      <ContactForm />
+      <Title headerTitle="Contacts" />
+      <Title headerTitle="Find contacts by name" />
+      <Filter />
+      <ContactsList />
+    </StyledWrapper>
   );
 };
+
+export default App;
