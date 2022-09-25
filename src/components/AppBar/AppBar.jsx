8b66a02a95b57c538f77558054488c611f7e46dc
serhiiarkhypiuk <airborn.gnus@gmail.com>
2022-09-25 20:55:32 +0300

    Modal updated

--- src/components/AppBar/AppBar.jsx
+++ src/components/AppBar/AppBar.jsx
@@ -1,11 +1,9 @@
 import { useSelector } from 'react-redux';
 import Navigation from './GeneralNavigation/GeneralNavigation';
-import UserMenu from './Menu/Menu';
 import { authSelectors } from 'redux/auth';
 
 const AppBar = () => {
   const isLoggedIn = useSelector(authSelectors.getLoggedIn);
-  const userName = useSelector(authSelectors.getUserName);
 
   return (
     <div>
